37774475d7739f4ae0a15918647db52fbc77a701
Elias Thompson <email@eliasthompson.com>
2019-09-15 17:53:13 -0700

    Baby go mode calculator.

--- scripts/functions.js
+++ scripts/functions.js
@@ -7,6 +7,8 @@ function toggleArtifacts(e) {
 
   if (e.which === 1 && unselected.length) unselected[0].classList.add('selected');
   if (e.which === 3 && selected.length) selected[selected.length - 1].classList.remove('selected');
+
+  checkGoMode();
 };
 
 function toggleItem(e) {
@@ -17,4 +19,32 @@ function toggleItem(e) {
 
   if (e.which === 1 && items.classList.contains('grayscale')) items.classList.remove('grayscale');
   if (e.which === 3 && !items.classList.contains('grayscale')) items.classList.add('grayscale');
+
+  checkGoMode();
+};
+
+function checkGoMode() {
+  var selectedArtifacts = document.querySelectorAll('.artifact.selected');
+  var unacquiredItems = document.querySelectorAll('.items.grayscale');
+  var goModeElement = document.querySelector('#go-mode');
+  let goMode = true;
+
+  if (selectedArtifacts.length !== 12) goMode = false;
+
+  if (goMode) {
+    unacquiredItems.forEach(function (item) {
+      console.log(item.children[0].getAttribute('src'));
+      if (goMode) {
+        if (item.children[0].getAttribute('src') === 'images/items/wavebeam.png') goMode = false;
+        if (item.children[0].getAttribute('src') === 'images/items/icebeam.png') goMode = false;
+        if (item.children[0].getAttribute('src') === 'images/items/plasmabeam.png') goMode = false;
+        if (item.children[0].getAttribute('src') === 'images/items/phazonsuit.png') goMode = false;
+      }
+    });
+  }
+
+  console.log(goMode);
+
+  if (goMode && goModeElement.classList.contains('grayscale')) goModeElement.classList.remove('grayscale');
+  else if (!goMode) goModeElement.classList.add('grayscale');
 };
